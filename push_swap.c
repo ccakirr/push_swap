a401b87b985104b88e085fc12a9fdc2204a87ebd
caner <caneroyku@outlook.com>
2025-09-22 16:03:57 +0300

    array to list converter added

--- push_swap.c
+++ push_swap.c
@@ -6,8 +6,18 @@
 /*   By: ccakir <ccakir@student.42istanbul.com.t    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/20 20:16:13 by ccakir            #+#    #+#             */
-/*   Updated: 2025/09/21 19:34:55 by ccakir           ###   ########.fr       */
+/*   Updated: 2025/09/22 16:01:06 by ccakir           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "push_swap.h"
 
+int	main(int ac, char **av[])
+{
+	t_list	stack_a;
+	t_list	stack_b;
+
+	(void *)ac;
+	stack_a = array_to_list(av);
+	error(stack_a);
+}
--- push_swap_utils.c
+++ push_swap_utils.c
@@ -6,22 +6,33 @@
 /*   By: ccakir <ccakir@student.42istanbul.com.t    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/20 23:06:10 by ccakir            #+#    #+#             */
-/*   Updated: 2025/09/21 23:12:18 by ccakir           ###   ########.fr       */
+/*   Updated: 2025/09/22 15:59:19 by ccakir           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-int	is_sorted(t_stack *b)
+t_list	*array_to_list(char **array)
 {
-	t_stack	*tmp;
-	
-	tmp = *b;
-	while((tmp -> next > tmp -> number) || (tmp -> next))
+	t_list	*head;
+	t_list	*current;
+	int		i;
+
+	head = NULL;
+	current = NULL;
+	i = 0;
+	while(array[i])
 	{
-		tmp = tmp -> next;
+		t_list	*new;
+		new = malloc((sizeof(t_list)));
+		new -> content = array[i];
+		new -> next = NULL;
+		if (!(head))
+			head = new;
+		else
+			current -> next = new;
+		current = new;
+		i++;
 	}
-	if(!(tmp->next))
-		return (1);
-	return (0);
+	return (head);
 }
