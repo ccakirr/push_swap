ae55e2c193ae46ca68df00937f18306873112ae7
caner <caneroyku@outlook.com>
2025-09-25 10:15:20 +0300

    swap.c edited leaks fixed push functions fixed

--- swap_funcs.c
+++ swap_funcs.c
@@ -6,66 +6,73 @@
 /*   By: ccakir <ccakir@student.42istanbul.com.t    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/21 19:35:10 by ccakir            #+#    #+#             */
-/*   Updated: 2025/09/25 10:04:32 by ccakir           ###   ########.fr       */
+/*   Updated: 2025/09/25 10:14:29 by ccakir           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include	"push_swap.h"
 
-void	sa(t_list	*stack_a)
+void	sa(t_list	**stack_a)
 {
 	t_list	*current;
 	t_list	*tmp;
 
-	current = malloc(sizeof(t_list));
-	tmp = malloc(sizeof(t_list));
-	current = stack_a->next;
-	tmp = stack_a;
-	stack_a->content = current->content;
+	current = (*stack_a)->next;
+	tmp = (*stack_a);
+	(*stack_a)->content = current->content;
 	current->content = tmp->content;
 	ft_printf("sa\n");
 }
 
-void sb(t_list	*stack_b)
+void sb(t_list	**stack_b)
 {
 	t_list	*current;
 	t_list	*tmp;
 
-	current = malloc(sizeof(t_list));
-	tmp = malloc(sizeof(t_list));
-	current = stack_b->next;
-	tmp = stack_b;
-	stack_b->content = current->content;
+	current = *(stack_b)->next;
+	tmp = *(stack_b);
+	*(stack_b)->content = current->content;
 	current->content = tmp->content;
-	ft_printf("sa\n");
+	ft_printf("sb\n");
 }
 
-void ss()
+void ss(t_list	**stack_a, t_list	**stack_b)
 {
 	t_list	*current;
 	t_list	*tmp;
 
-	current = malloc(sizeof(t_list));
-	tmp = malloc(sizeof(t_list));
-	current = stack_a->next;
-	tmp = stack_a;
-	stack_a->content = current->content;
+	current = (*stack_a)->next;
+	tmp = (*stack_a);
+	(*stack_a)->content = current->content;
 	current->content = tmp->content;
-	current = stack_b->next;
-	tmp = stack_b;
-	stack_b->content = current->content;
+	current = (*stack_b)->next;
+	tmp = (*stack_b);
+	(*stack_b)->content = current->content;
 	current->content = tmp->content;
 	ft_printf("ss\n");
 }
 
-void	pa(t_list	*stack_a, t_list	*stack_b)
+void	pa(t_list **stack_a, t_list **stack_b)
 {
-	ft_lstadd_front(stack_b, stack_a);
+	t_list *tmp;
+
+	if (!*stack_b)
+		return ;
+	tmp = *stack_b;
+	*stack_b = (*stack_b)->next;
+	tmp->next = *stack_a;
+	*stack_a = tmp;
 	ft_printf("pa\n");
 }
 
-void	pb(t_list	*stack_a,	t_list	*stack_b)
-{
-	ft_lstadd_front(stack_a, stack_b);
+void	pb(t_list **stack_a, t_list **stack_b)
+{	t_list *tmp;
+
+	if (!*stack_a)
+		return ;
+	tmp = *stack_a;
+	*stack_a = (*stack_a)->next;
+	tmp->next = *stack_b;
+	*stack_b = tmp;
 	ft_printf("pb\n");
 }
