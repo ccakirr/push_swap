904b2c2a0b4e1f4ba57af19ebf0bca0e43182b57
caner <caneroyku@outlook.com>
2025-09-25 10:05:12 +0300

    push functions added

--- swap_funcs.c
+++ swap_funcs.c
@@ -6,9 +6,11 @@
 /*   By: ccakir <ccakir@student.42istanbul.com.t    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/21 19:35:10 by ccakir            #+#    #+#             */
-/*   Updated: 2025/09/22 19:58:01 by ccakir           ###   ########.fr       */
+/*   Updated: 2025/09/25 10:04:32 by ccakir           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include	"push_swap.h"
 
 void	sa(t_list	*stack_a)
 {
@@ -56,12 +58,14 @@ void ss()
 	ft_printf("ss\n");
 }
 
-void	pa()
+void	pa(t_list	*stack_a, t_list	*stack_b)
 {
-	
+	ft_lstadd_front(stack_b, stack_a);
+	ft_printf("pa\n");
 }
 
-void	pb()
+void	pb(t_list	*stack_a,	t_list	*stack_b)
 {
-	
+	ft_lstadd_front(stack_a, stack_b);
+	ft_printf("pb\n");
 }
