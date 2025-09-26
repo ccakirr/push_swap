638f9cc0d511178173a245f1c7d580e9d46a4bbc
caner <caneroyku@outlook.com>
2025-09-26 16:31:20 +0300

    r swap funcs added.

--- r_swap_funcs.c
+++ r_swap_funcs.c
@@ -6,21 +6,59 @@
 /*   By: ccakir <ccakir@student.42istanbul.com.t    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/21 19:38:34 by ccakir            #+#    #+#             */
-/*   Updated: 2025/09/21 19:40:51 by ccakir           ###   ########.fr       */
+/*   Updated: 2025/09/26 16:30:28 by ccakir           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-void	ra()
-{
+#include "push_swap.h"
 
+void	ra(t_list	*stack_a)
+{
+	t_list	*tmp;
+	t_list	*last;
+	t_list	*current;
+
+	tmp = stack_a;
+	current = stack_a;
+	while(tmp->next)
+	{
+		tmp = tmp->next;
+	}
+	last = tmp;
+	tmp->next = stack_a;
+	while(current->next != last)
+	{
+		current = current->next;
+	}
+	current->next = NULL;
+	ft_printf("ra\n");
 }
 
-void	rb()
+void	rb(t_list	*stack_b)
 {
-	
+	t_list	*tmp;
+	t_list	*last;
+	t_list	*current;
+
+	tmp = stack_b;
+	current = stack_b;
+	while(tmp->next)
+	{
+		tmp = tmp->next;
+	}
+	last = tmp;
+	tmp->next = stack_b;
+	while(current->next != last)
+	{
+		current = current->next;
+	}
+	current->next = NULL;
+	ft_printf("rb\n");
 }
 
-void	rr()
+void	rr(t_list	*stack_a, t_list	*stack_a)
 {
-	
+	rb(stack_b);
+	ra(stack_a);
+	ft_printf("rr\n");
 }
