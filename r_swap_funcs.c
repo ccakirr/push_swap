40c2e7c17dddf2b38c3d9b426719ef24799ad989
caner <caneroyku@outlook.com>
2025-09-29 14:14:10 +0300

    r_swap functions edited and reverse_r_swap functions added and there is a note for future to using alghoritm

--- r_swap_funcs.c
+++ r_swap_funcs.c
@@ -6,59 +6,66 @@
 /*   By: ccakir <ccakir@student.42istanbul.com.t    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/21 19:38:34 by ccakir            #+#    #+#             */
-/*   Updated: 2025/09/26 16:30:28 by ccakir           ###   ########.fr       */
+/*   Updated: 2025/09/29 14:09:24 by ccakir           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-void	ra(t_list	*stack_a)
+void	ra(t_list	**stack_a)
 {
 	t_list	*tmp;
 	t_list	*last;
-	t_list	*current;
 
-	tmp = stack_a;
-	current = stack_a;
-	while(tmp->next)
-	{
-		tmp = tmp->next;
-	}
-	last = tmp;
-	tmp->next = stack_a;
-	while(current->next != last)
-	{
-		current = current->next;
-	}
-	current->next = NULL;
+	if (!*stack_a || !(*stack_a)->next)
+		return ;
+	tmp = *stack_a;
+	*stack_a = (*stack_a)->next;
+	last = *stack_a;
+	while (last->next)
+		last = last->next;
+	last->next = tmp;
+	tmp->next = NULL;
 	ft_printf("ra\n");
 }
 
-void	rb(t_list	*stack_b)
+void	rb(t_list	**stack_b)
 {
 	t_list	*tmp;
 	t_list	*last;
-	t_list	*current;
 
-	tmp = stack_b;
-	current = stack_b;
-	while(tmp->next)
-	{
-		tmp = tmp->next;
-	}
-	last = tmp;
-	tmp->next = stack_b;
-	while(current->next != last)
-	{
-		current = current->next;
-	}
-	current->next = NULL;
-	ft_printf("rb\n");
+	if (!*stack_a || !(*stack_a)->next)
+		return ;
+	tmp = *stack_a;
+	*stack_a = (*stack_a)->next;
+	last = *stack_a;
+	while (last->next)
+		last = last->next;
+	last->next = tmp;
+	tmp->next = NULL;
+	ft_printf("ra\n");
 }
 
-void	rr(t_list	*stack_a, t_list	*stack_a)
+void	rr(t_list	**stack_a, t_list	**stack_a)
 {
-	rb(stack_b);
-	ra(stack_a);
+	t_list	*tmp;
+	t_list	*last;
+
+	if ((!*stack_a || !(*stack_a)->next) || (!*stack_a || !(*stack_a)->next))
+		return ;
+	tmp = *stack_a;
+	*stack_a = (*stack_a)->next;
+	last = *stack_a;
+	while (last->next)
+		last = last->next;
+	last->next = tmp;
+	tmp->next = NULL;
+	tmp = *stack_a;
+	*stack_a = (*stack_a)->next;
+	last = *stack_a;
+	while (last->next)
+		last = last->next;
+	last->next = tmp;
+	tmp->next = NULL;
 	ft_printf("rr\n");
 }
--- reverse_r_swap_funcs.c
+++ reverse_r_swap_funcs.c
@@ -6,21 +6,74 @@
 /*   By: ccakir <ccakir@student.42istanbul.com.t    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/21 19:41:07 by ccakir            #+#    #+#             */
-/*   Updated: 2025/09/21 19:41:47 by ccakir           ###   ########.fr       */
+/*   Updated: 2025/09/29 14:07:13 by ccakir           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-void	rra()
+#include "push_swap.h"
+
+void	rra(t_list	**stack_a)
 {
-	
+	t_list	*tmp;
+	t_list	*last;
+
+	if (!*stack_a || !(*stack_a)->next)
+		return ;
+	tmp = *stack_a;
+	last = *stack_a;
+	while (last->next)
+		last = last->next;
+	last->next = (*stack_a);
+	while (tmp->next != last)
+		tmp = tmp->next;
+	tmp->next = NULL;
+	*stack_a = last;
+	ft_printf("rra\n");
 }
 
-void	rrb()
+void	rrb(t_list	**stack_b)
 {
-	
+	t_list	*tmp;
+	t_list	*last;
+
+	if(!*stack_b || !(*stack_b)->next)
+		return ;
+	tmp = *stack_b;
+	last = *stack_b;
+	while(last->next)
+		last = last->next;
+	last->next = *stack_b;
+	while(tmp->next != last)
+		tmp = tmp->next;
+	tmp->next = NULL;
+	*stack_b = last;
+	ft_printf("rrb\n");
 }
 
-void	rrr()
+void	rrr(t_list	**stack_a, t_list	**stack_b)
 {
-	
+	t_list	*tmp;
+	t_list	*last;
+
+	if ((!*stack_a || !(*stack_a)->next) || (!*stack_b || !(*stack_b)->next)) 
+		return ;
+	tmp = *stack_a;
+	last = *stack_a;
+	while (last->next)
+		last = last->next;
+	last->next = (*stack_a);
+	while (tmp->next != last)
+		tmp = tmp->next;
+	tmp->next = NULL;
+	*stack_a = last;
+	tmp = *stack_b;
+	last = *stack_b;
+	while(last->next)
+		last = last->next;
+	last->next = *stack_b;
+	while(tmp->next != last)
+		tmp = tmp->next;
+	tmp->next = NULL;
+	*stack_b = last;
+	ft_printf("rrr\n");
 }
